7e42e66c5a31b6e4b8af6a8e5a7231fd10026d40
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-05-01 21:42:26 -0300

    att

--- clib/open.c
+++ clib/open.c
@@ -1,4 +1,17 @@
 
+#include "dependencies/LuaCEmbed.h"
+#include "dependencies/doTheWorld.h"
+
+
+LuaCEmbedNamespace  lua;
+DtwNamespace dtw;
+
+int luaopen_doTheWorld(lua_State *state){
+    lua = newLuaCEmbedNamespace();
+    dtw = newDtwNamespace();
+    bool set_functions_as_public  = false;
+    LuaCEmbed * l  = lua.newLuaLib(state,set_functions_as_public);
 
 
 
+}
