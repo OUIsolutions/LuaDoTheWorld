4606cc4b845ec050ff8e756bdb6238dcabe8b2f2
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-05-01 23:55:52 -0300

    att

--- build.lua
+++ build.lua
@@ -1,11 +1,2 @@
-
--- means its linux
-if os.getenv("HOME") then
-    os.execute("gcc -Wall -shared -fpic -o luaDoTheWorld/linuxlib.so  clib/main.c")
-end 
---means its windows
-if  package.config:sub(1,1) == '\\' then
-    print("Sistema Windows")
-end
-
+os.execute("gcc -Wall -shared -fpic -o luaDoTheWorld/linuxlib.so  clib/main.c")
 
