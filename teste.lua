c7f6757ce5ab5548878d43a015be842d61484b78
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-05-03 02:42:41 -0300

    att

--- teste.lua
+++ teste.lua
@@ -4,5 +4,4 @@ dtw = require("luaDoTheWorld/luaDoTheWorld")
 local r = dtw.load_file("deer.jpg")
 dtw.write_file("saida.jpg",r)
 
-local sha  = dtw.generate_sha(r)
-print(sha)
+local sha  = dtw.generate_sha_from_file(10)
