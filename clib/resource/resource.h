1b7c9996a7c89e5bf5584fef2e12b5158142672e
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-05-04 14:34:20 -0300

    at

--- clib/resource/resource.h
+++ clib/resource/resource.h
@@ -2,23 +2,31 @@
 
 LuaCEmbedResponse * resource_to_string(LuaCEmbedTable  *self,LuaCEmbed *args);
 
+
 LuaCEmbedResponse * resource_to_number(LuaCEmbedTable  *self,LuaCEmbed *args);
+
 
 LuaCEmbedResponse * resource_to_boolean(LuaCEmbedTable  *self,LuaCEmbed *args);
 
+
 LuaCEmbedResponse * resource_sub_resource(LuaCEmbedTable  *self,LuaCEmbed *args);
+
+
 
 LuaCEmbedResponse * resource_value(LuaCEmbedTable  *self,LuaCEmbed *args);
 
+
 LuaCEmbedResponse * resource_set_value(LuaCEmbedTable  *self,LuaCEmbed *args);
 
+
 LuaCEmbedResponse * resource_commit(LuaCEmbedTable  *self,LuaCEmbed *args);
+
 
 LuaCEmbedResponse * resource_destroy(LuaCEmbedTable  *self,LuaCEmbed *args);
 
 
-
 LuaCEmbedResponse * free_resource(LuaCEmbedTable  *self, LuaCEmbed *args);
+
 
 LuaCEmbedTable *raw_create_resource(LuaCEmbed *args,DtwResource *resource);
 
