81c7220b34633efb0afd3f9238b6fd460b6a6417
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-05-06 14:22:28 -0300

    att

--- teste.lua
+++ teste.lua
@@ -5,6 +5,7 @@ local dtw = require("luaDoTheWorld/luaDoTheWorld")
 
 local value = dtw.newResource("a")
 local b = value.sub_resource("b.txt")
+b.set_value("aaaaaaaaaaaa")
 b.set_extension("json")
 value.commit()
 
