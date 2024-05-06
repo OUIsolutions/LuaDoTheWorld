7dba6a1fedf6907151c6c97557d73fa295dd066c
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-05-06 09:58:32 -0300

    att

--- build.lua
+++ build.lua
@@ -1,4 +1,5 @@
 
+local dtw = require("luaDoTheWorld/luaDoTheWorld")
 
 
 local RECONSTRUCT = false
@@ -19,6 +20,7 @@ local function slipt (inputstr, sep)
 end
 
 
+
 local function handle_side_effect(dir,current_assignature) 
 
     local side_effect_expected_folder = dir.."/side_effect"
@@ -34,54 +36,29 @@ local function handle_side_effect(dir,current_assignature)
         local expected_assignature = tostring(expected_hasher) 
         if expected_assignature ~= current_assignature then
             print("side effect diferent")
-            dtw.copy_any_overwriting(side_effect_copy_path,SIDE_EFFECT);
-            os.exit(1)
+            return 1
         end
     end
 
-
-
-
 end
 
-print("compiling")
 
-local r,e,code = os.execute("gcc -Wall -shared -fpic -o luaDoTheWorld/luaDoTheWorld_clib.so  clib/main.c")
-
-if code == 1 then
-    os.exit(1)
-end
-
-local dtw = require("luaDoTheWorld/luaDoTheWorld")
-local concat_path = true;
-local tests  = dtw.list_dirs("tests",concat_path)
-
-
-
-local hasher = dtw.newHasher()
-hasher.digest_folder_by_content(SIDE_EFFECT)
-local start_assignature = tostring(hasher)
-
-dtw.copy_any_overwriting(SIDE_EFFECT,side_effect_copy_path)
-
-
-for i, t in ipairs(tests) do 
+---@param unit string
+---@param start_assignature string 
+local function test_unit(unit,start_assignature)
+    print("testing: "..unit)
     
-    print("testing: "..t)
-
     local name = slipt(t,"/")[2]
     if name == nil then 
-        print("name of test "..t.."not provide")
-        os.exit(1)
+        print("name of test "..unit.."not provide")
+        return true 
     end 
-    local file_path = t..name..".lua"
-
-
-    local expected_file_path = t.."expected.txt"
+    
+    local file_path = unit..name..".lua"  
+    local expected_file_path = unit.."expected.txt"
     
 
-
-
+    -- verifying expected code 
     local expected_code = dtw.load_file(expected_file_path)
     if expected_code == nil or RECONSTRUCT then 
         print(" creatad "..file_path.." ")
@@ -91,10 +68,9 @@ for i, t in ipairs(tests) do
         end
 
     end 
-
-
     local expected_code = dtw.load_file(expected_file_path)
     
+
 
     local comparation_result = io.popen("lua "..file_path,"r"):read()
     if expected_code ~=comparation_result then
@@ -103,8 +79,7 @@ for i, t in ipairs(tests) do
             .." was expecting:'"..expected_code.."'\n"
             .."but got:'"..comparation_result.."'")
         --revert any modification    
-        dtw.copy_any_overwriting(side_effect_copy_path,SIDE_EFFECT);
-        os.exit(1)
+        return  true
     end
 
     --VERIFY IF SOMETHING WERE MODIFIED
@@ -115,8 +90,48 @@ for i, t in ipairs(tests) do
 
     --means code generated side effect
     if start_assignature ~= test_assignature then
-        handle_side_effect(t,test_assignature)
+        local error = handle_side_effect(unit,test_assignature)
+        if error then
+            return error
+        end
     end
     
     --validate_commad_result(result)
+
+end
+
+
+---@param start_assignature string 
+local function exec_tests(start_assignature)
+    print("starting tests")
+
+    local concat_path = true;
+    local tests  = dtw.list_dirs("tests",concat_path)
+    
+    for i, t in ipairs(tests) do 
+        test_unit(t,start_assignature)
+    end 
+
 end 
+
+local function main()
+
+    print("compiling")
+
+    local r,e,code = os.execute("gcc -Wall -shared -fpic -o luaDoTheWorld/luaDoTheWorld_clib.so  clib/main.c")
+    
+    if code == 1 then
+        return
+    end
+
+    local hasher = dtw.newHasher()
+    hasher.digest_folder_by_content(SIDE_EFFECT)
+    local start_assignature = tostring(hasher)    
+    dtw.copy_any_overwriting(SIDE_EFFECT,side_effect_copy_path)
+    
+    exec_tests(start_assignature)
+
+    dtw.copy_any_overwriting(side_effect_copy_path,SIDE_EFFECT);
+    dtw.remove_any(side_effect_copy_path)
+end
+main()
