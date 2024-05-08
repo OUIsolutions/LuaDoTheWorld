27d04e65747f691ca53abe71ba57beefdfd7d844
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-05-08 11:02:53 -0300

    att

--- teste.lua
+++ teste.lua
@@ -1,8 +1,19 @@
 
 
 
+
+
+
+
 local dtw = require("luaDoTheWorld/luaDoTheWorld")
-
-local r  = dtw.newResource("a").sub_resource(1).set_value("aaa")
-
-r.commit()
+local banco = dtw.newResource("banco")
+function encontra_ususario (nome)
+    local todos_usuarios = banco.sub_resource("usuarios")
+    local index = 1
+    while true do
+    local possivel_usuario = todos_usuarios.sub_resource (index)   
+    local possive_nome = possivel_usuario.sub_resource ("nome")
+        print(possivel_nome)
+    end 
+end
+encontra_ususario ("Mateus")
