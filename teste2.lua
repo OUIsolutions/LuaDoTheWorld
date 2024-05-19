1924975d7e39f6023685cb2928c981cf3b4fa844
Mateus <mateusmoutinho01@gmail.com>
2024-05-19 05:59:50 -0300

    at

--- teste2.lua
+++ teste2.lua
@@ -2,20 +2,7 @@
 local dtw = require("luaDoTheWorld/luaDoTheWorld")
 
 
-local v = dtw.load_file("teste.jpg")
-
-local i = 1 
-local current = v[i]
-local repetitions = 0
-while current  do
-
-    local old = v[i-1]
-    current = v[i]
-
-    if old == current then
-        repetitions = repetitions+1
-    end
-    i = i+1
+local e = dtw.list_files_recursively("clib")
+for i,v in pairs(e) do
+    print(v)
 end
-print("total",i)
-print("repetitions",repetitions)
