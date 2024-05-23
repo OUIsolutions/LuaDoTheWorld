801e8386b03a8647a5374bea0075f1292b289d44
Mateus <mateusmoutinho01@gmail.com>
2024-05-23 08:34:53 -0300

    att

--- teste2.lua
+++ teste2.lua
@@ -7,8 +7,8 @@ local schema = banco.newSchema()
 local users_schema =schema.sub_schema("users")
 users_schema.add_primary_keys({"name","email"})
 
+
 local users = banco.sub_resource("users")
-
-local mateus = users.get_resource_matching_primary_key("name","mateus")
-
-print(mateus["name"])
+users.each_schema(function (value)
+    print(value)
+end)
