db474f3fdfa76402dd8b68a1962571595716eac7
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-06-02 05:27:53 -0300

    att

--- clib/tree/tree/tree.c
+++ clib/tree/tree/tree.c
@@ -69,24 +69,106 @@ LuaCEmbedResponse *tree_get_tree_part_by_path(LuaCEmbedTable *self, LuaCEmbed *a
 
 
 LuaCEmbedResponse *tree_find(LuaCEmbedTable *self, LuaCEmbed *args){
-    return  NULL;
+    DtwTree *self_tree = (DtwTree*)lua.tables.get_long_prop(self,TREE_POINTER);
+
+    for(int i =0; i < self_tree->size;i++){
+        LuaCEmbedTable *tree_part = create_tree_part_reference(args,self_tree->tree_parts[i]);
+        LuaCEmbedTable *args_to_call = lua.tables.new_anonymous_table(args);
+        lua.tables.append_table(args_to_call,tree_part);
+        LuaCEmbedTable *response = lua.args.run_lambda(args,0,args_to_call,1);
+        if(lua.has_errors(args)){
+            char *error_msg = lua.get_error_message(args);
+            return lua.response.send_error(error_msg);
+        }
+        int response_type =lua.tables.get_type_by_index(response,0);
+        if(response_type != lua.types.NILL){
+            bool is_the_value = lua.tables.get_bool_by_index(response,0);
+            if(lua.has_errors(args)){
+                char *error_msg = lua.get_error_message(args);
+                return lua.response.send_error(error_msg);
+            }
+
+            if(is_the_value){
+                return  lua.response.send_table(tree_part);
+            }
+        }
+    }
+    return NULL;
 }
 
 
 LuaCEmbedResponse *tree_count(LuaCEmbedTable *self, LuaCEmbed *args){
-    return  NULL;
+    DtwTree *self_tree = (DtwTree*)lua.tables.get_long_prop(self,TREE_POINTER);
+    long total = 0;
+    for(int i =0; i < self_tree->size;i++){
+        LuaCEmbedTable *tree_part = create_tree_part_reference(args,self_tree->tree_parts[i]);
+        LuaCEmbedTable *args_to_call = lua.tables.new_anonymous_table(args);
+        lua.tables.append_table(args_to_call,tree_part);
+        LuaCEmbedTable *response = lua.args.run_lambda(args,0,args_to_call,1);
+        if(lua.has_errors(args)){
+            char *error_msg = lua.get_error_message(args);
+            return lua.response.send_error(error_msg);
+        }
+        int response_type =lua.tables.get_type_by_index(response,0);
+        if(response_type != lua.types.NILL){
+            bool is_the_value = lua.tables.get_bool_by_index(response,0);
+            if(lua.has_errors(args)){
+                char *error_msg = lua.get_error_message(args);
+                return lua.response.send_error(error_msg);
+            }
+
+            if(is_the_value){
+                total+=1;
+            }
+        }
+
+    }
+    return lua.response.send_long(total);
 }
 
 
 LuaCEmbedResponse *tree_map(LuaCEmbedTable *self, LuaCEmbed *args){
-    return  NULL;
+
+    DtwTree *self_tree = (DtwTree*)lua.tables.get_long_prop(self,TREE_POINTER);
+    LuaCEmbedTable *final_map = lua.tables.new_anonymous_table(args);
+
+    for(int i =0; i < self_tree->size;i++){
+        LuaCEmbedTable *tree_part = create_tree_part_reference(args,self_tree->tree_parts[i]);
+        LuaCEmbedTable *args_to_call = lua.tables.new_anonymous_table(args);
+        lua.tables.append_table(args_to_call,tree_part);
+        LuaCEmbedTable *response = lua.args.run_lambda(args,0,args_to_call,1);
+        if(lua.has_errors(args)){
+            char *error_msg = lua.get_error_message(args);
+            return lua.response.send_error(error_msg);
+        }
+        int response_type =lua.tables.get_type_by_index(response,0);
+        if(response_type!= LUA_CEMBED_NIL && response_type != LUA_CEMBED_NOT_FOUND){
+            lua.tables.append_evaluation(final_map,"%s[1]",response->global_name);
+        }
+
+    }
+    return lua.response.send_table(final_map);
 }
 
 
 
 
 LuaCEmbedResponse *tree_foreach(LuaCEmbedTable *self, LuaCEmbed *args){
-    return  NULL;
+
+    DtwTree *self_tree = (DtwTree*)lua.tables.get_long_prop(self,TREE_POINTER);
+
+    for(int i =0; i < self_tree->size;i++){
+        LuaCEmbedTable *tree_part = create_tree_part_reference(args,self_tree->tree_parts[i]);
+        LuaCEmbedTable *args_to_call = lua.tables.new_anonymous_table(args);
+        lua.tables.append_table(args_to_call,tree_part);
+        lua.args.run_lambda(args,0,args_to_call,1);
+        if(lua.has_errors(args)){
+            char *error_msg = lua.get_error_message(args);
+            return lua.response.send_error(error_msg);
+        }
+
+    }
+    return NULL;
 }
 
 LuaCEmbedResponse *insecure_hardware_write_tree(LuaCEmbedTable *self, LuaCEmbed *args){
@@ -142,7 +224,6 @@ LuaCEmbedResponse *tree_dump_to_json_file(LuaCEmbedTable *self, LuaCEmbed *args)
 LuaCEmbedTable * raw_create_tree(LuaCEmbed *args,DtwTree *tree){
     LuaCEmbedTable *self = lua.tables.new_anonymous_table(args);
     lua.tables.set_long_prop(self,TREE_POINTER,(long long)tree);
-    lua.tables.set_method(self,ADD_TREE_PART_METHOD,tree_add_part);
     lua.tables.set_method(self,GET_TREE_PART_BY_INDEX_METHOD,get_tree_part_by_index);
     lua.tables.set_method(self,INSECURE_HARDWARE_WRITE_TREE,insecure_hardware_write_tree);
     lua.tables.set_method(self,INSECURE_HARDWARE_REMOVE_TREE,insecure_hardware_remove_tree);
