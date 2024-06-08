6d756782582077729649d909f86ba1f9d92f4926
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-06-08 00:47:14 -0300

    at

--- build/lua/main.lua
+++ build/lua/main.lua
@@ -7,10 +7,6 @@ local function main()
         local src_sha = Generate_sha_from_folder_not_considering_empty_folders(LIB_FOLDER)
         local cache = NewCache(CACHE_POINT)
 
-        local amalgamation_cache = cache.new_element("amalgamation",function ()
-            return Generate_amalgamation_recursive(START_POINT)
-        end).add_dependencie(src_sha)
-
 
         local amalgamation_result = amalgamation_cache.perform()
         dtw.write_file(END_TEST_POINT,amalgamation_result)
