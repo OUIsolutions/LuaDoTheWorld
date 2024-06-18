ad26dd4cfce866330f1639b422895a53ccb4835b
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-06-18 15:43:30 -0300

    at

--- teste.lua
+++ teste.lua
@@ -10,6 +10,13 @@ local fork = dtw.newFork(function ()
 	end
 end)
 
-fork.wait(30000)
+local fork2 = dtw.newFork(function ()
+	while true do
+
+	end
+end)
+
+fork.wait(3000)
+fork2.wait(3000)
 
 print("fora")
