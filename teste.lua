6baa71182991f5481567dc218658b34a790905f9
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-06-20 13:54:16 -0300

    att

--- teste.lua
+++ teste.lua
@@ -1,18 +1,11 @@
 local dtw = require("luaDoTheWorld/luaDoTheWorld")
 
-local tree = dtw.newTree_from_hardware("tests/target/test_dir")
+local tree = dtw.newTree()
+local a = tree.newTreePart_empty("tests/target/Tree/a.txt")
+a.set_value("content of a")
+a.hardware_write()
+local b = tree.newTreePart_empty("tests/target/Tree/b.txt")
+a.set_value("content of b")
+a.hardware_write()
 
-local result = tree.map(function (element)
-
-	return {path=element.path.get_full_path(),content=element.get_value()}
-
-end)
-
-local size = tree.get_size()
-for i=1,size do
-    local current  = result[i]
-    print("========================")
-    print("path",current.path)
-    print("content",current.content)
-
-end
+tree.commit()
