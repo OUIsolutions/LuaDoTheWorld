c1243bf7fe6c42488f0fe0b672d23333e6267d5b
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-06-23 14:00:39 -0300

    att

--- tests/main_test/Tree/T_filter/exec.lua
+++ tests/main_test/Tree/T_filter/exec.lua
@@ -4,14 +4,13 @@ local tree = dtw.newTree_from_hardware("tests/target/test_dir")
 
 local result,size = tree.filter(function (element)
 
-    if(element.get_value())
-	return {path=element.path.get_full_path(),content=element.get_value()}
+    if(element.get_value() ~=nil)then
+    return true
+    end
 
 end)
 for i=1,size do
-    local current  = result[i]
-    print("========================")
-    print("path",current.path)
-    print("content",current.content)
-
+    local current = result[i]
+    print("path",current.path.get_full_path())
+    print("content",current.path.get_value())
 end
