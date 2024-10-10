0a3925762ab92659afdf5abc4b05245f86cdf257
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-10-10 18:13:04 -0300

    att

--- teste.lua
+++ teste.lua
@@ -11,9 +11,18 @@ for i = 1, size do
         dtw.remove_any(current)
     end
     local dir = path.get_dir()
+    local extension = path.get_extension()
     if dir == "src/constants/" then
-        value_to_add = "consts" .. name
-        path.set_name(name)
+        value_to_add = "consts." .. name
+        path.set_name(value_to_add)
+        dtw.move_any_overwriting(current, path.get_full_path())
+    elseif extension == "c" then
+        value_to_add = "fdefine." .. name
+        path.set_name(value_to_add)
+        dtw.move_any_overwriting(current, path.get_full_path())
+    elseif extension == "h" then
+        value_to_add = "fdeclare." .. name
+        path.set_name(value_to_add)
         dtw.move_any_overwriting(current, path.get_full_path())
     end
 end
