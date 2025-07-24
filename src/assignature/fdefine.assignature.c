dad055bc9c6ba15ef368973d4142e2e9d13c56aa
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-07-24 18:09:18 -0300

    att

--- src/assignature/fdefine.assignature.c
+++ src/assignature/fdefine.assignature.c
@@ -9,7 +9,7 @@ void ldtw_digest_table(LuaCEmbedTable *table,DtwHash *hasher){
     long size  = LuaCEmbedTable_get_full_size(table);
     for(int i = 0; i < size;i++){
         bool has_key = LuaCembedTable_has_key_at_index(table, i);
-        if (!has_key) {
+        if (has_key) {
             continue;
         }
         int type_item = LuaCEmbedTable_get_type_by_index(table, i);
@@ -43,8 +43,53 @@ void ldtw_digest_table(LuaCEmbedTable *table,DtwHash *hasher){
                 break;
         }
     }
-
+    DtwStringArray *keys = newDtwStringArray();
+    for(int i = 0; i < size;i++){
+        bool has_key = LuaCembedTable_has_key_at_index(table, i);
+        if (!has_key) {
+            continue;
+        }
+        char *key = LuaCEmbedTable_get_string_by_index(table, i);
+        DtwStringArray_append(keys,key);
+    }
+    DtwStringArray_sort(keys);
+    for(int i = 0; i < keys->size;i++){
+        char *key = keys->strings[i];
+        printf("key is %s\n", key);
+        DtwHash_digest_string(hasher, key);
     
+        int type = LuaCEmbedTable_get_type_prop(table, key);
+        switch (type) {
+            case LUA_CEMBED_STRING: {
+                char *str = LuaCembedTable_get_string_prop(table, key);
+                DtwHash_digest_string(hasher, str);     
+                break;
+            }
+            case LUA_CEMBED_BOOL: {
+                bool b = LuaCembedTable_get_bool_prop(table, key);
+                DtwHash_digest_bool(hasher, b);
+                break;
+            }
+            case LUA_CEMBED_NUMBER: {
+                double d = LuaCembedTable_get_double_prop(table, key);
+                DtwHash_digest_double(hasher, d);
+                break;
+            }   
+            case LUA_CEMBED_NIL: {
+                DtwHash_digest_string(hasher, "nil");
+                break;
+            }
+            case LUA_CEMBED_TABLE: {
+                LuaCEmbedTable *sub_table = LuaCEmbedTable_get_sub_table_by_key(table, key);
+                ldtw_digest_table(sub_table, hasher);
+                break;
+            }
+            default:
+                // Unknown type, skip
+                break;
+        }
+    }
+    DtwStringArray_free(keys);
 }
 
 LuaCEmbedResponse *ldtw_assign_element(LuaCEmbed *args){
