25ff55a59825e520e826d3b39dc5c7151f3af556
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-07-24 22:41:12 -0300

    creating thes seralization mechanic

--- src/serializer/fdefine.serializer.c
+++ src/serializer/fdefine.serializer.c
@@ -16,7 +16,40 @@ void ldtw_serialize_str(privateLuaDtwStringAppender *appender,unsigned char *str
 
 }
 void ldtw_serialize_table(privateLuaDtwStringAppender *appender,LuaCEmbedTable *table){
-
+    privateLuaDtwStringAppender_append(appender, " {");
+    lua_Integer size = LuaCEmbedTable_get_full_size(table);
+    for(lua_Integer i = 0; i < size; i++){
+        if(i > 0) privateLuaDtwStringAppender_append(appender, ", ");
+        // Serialize key if present
+ 
+        if(LuaCembedTable_has_key_at_index(table, i)){
+            char *key = LuaCembedTable_get_key_by_index(table, i);
+            privateLuaDtwStringAppender_append(appender, "[");
+            ldtw_serialize_str(appender, (unsigned char*)key, strlen(key));
+            privateLuaDtwStringAppender_append(appender, "] = ");
+        }
+        // Serialize value
+        int type = LuaCEmbedTable_get_type_by_index(table, i);
+        if(type == LUA_CEMBED_STRING){
+            lua_Integer str_size;
+            unsigned char *str = (unsigned char*)LuaCEmbedTable_get_raw_string_by_index(table, i, &str_size);
+            ldtw_serialize_str(appender, str, str_size);
+        }else if(type == LUA_CEMBED_NUMBER){
+            double num = LuaCEmbedTable_get_double_by_index(table, i);
+            privateLuaDtwStringAppender_append_fmt(appender, "%f", num);
+        }else if(type == LUA_CEMBED_BOOL){
+            bool b = LuaCEmbedTable_get_bool_by_index(table, i);
+            privateLuaDtwStringAppender_append(appender, b ? "true" : "false");
+        }else if(type == LUA_CEMBED_NIL){
+            privateLuaDtwStringAppender_append(appender, "nil");
+        }else if(type == LUA_CEMBED_TABLE){
+            LuaCEmbedTable *sub = LuaCEmbedTable_get_sub_table_by_index(table, i);
+            ldtw_serialize_table(appender, sub);
+        }else{
+            privateLuaDtwStringAppender_append(appender, "nil"); // fallback for unknown types
+        }
+    }
+    privateLuaDtwStringAppender_append(appender, " }");
 }
 
 LuaCEmbedResponse *ldtw_serialize_var(LuaCEmbed *args){
