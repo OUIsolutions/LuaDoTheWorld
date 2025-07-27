1574b409555658f1890b498d76c206c049c18f18
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-07-26 22:02:55 -0300

    att

--- src/constants/consts.func_names.h
+++ src/constants/consts.func_names.h
@@ -33,6 +33,7 @@
 #define IS_FILE_FUNC "isfile"
 #define IS_FILE_BLOB "isfile_blob"
 #define CREATE_CACHE_FUNCTION "create_cache_function"
+#define CACHE_EXECUTE "execute_cache"
 #define CLEAR_CACHE "clear_cache"
 #define GENERATE_SHA_FROM_FOLDER_BY_CONTENT "generate_sha_from_folder_by_content"
 #define GENERATE_SHA_FROM_FOLDER_BY_LAST_MODIFICATION "generate_sha_from_folder_by_last_modification"
