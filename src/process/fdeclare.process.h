89e0f9b81dd8cbed0a98d077361af174a0c7a642
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-08-16 21:40:00 -0300

    att

--- src/process/fdeclare.process.h
+++ src/process/fdeclare.process.h
@@ -2,3 +2,7 @@
 //mannaged by silver chain: https://github.com/OUIsolutions/SilverChain
 #include "../imports/imports.type.h"
 //silver_chain_scope_end
+
+LuaCEmbedResponse  * dtw_get_pid(LuaCEmbed *args);
+
+LuaCEmbedResponse *dtw_is_pid_alive(LuaCEmbed *args);
